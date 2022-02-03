838c022f0207ec86e71ce677fec5be34eca9d5d8
MohammadHakemi <hakemi.mohammad.22@gmail.com>
2022-02-03 20:29:50 +0330

    initial BoltDb database

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
-import "task/cmd"
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"task/cmd"
+	"task/db"
+)
+
+func main() {
+	homedir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	dbPath := filepath.Join(homedir, "tasks.db")
+	fmt.Println(dbPath)
+	must(db.InitDb(dbPath))
+	must(cmd.RootCmd.Execute())
+
+}
 
 
-func main(){
-	cmd.RootCmd.Execute()
-
+func must(err error){
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
